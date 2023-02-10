fd901cc01e961e72530711eb281ac89159414451
KnightDanny <122959926+KnightDanny@users.noreply.github.com>
2023-02-10 15:56:25 +0300

    Add files via upload

---
+++ Valid Parentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s):
+    	size = len(s)
+    	pre = 0
+    	while pre != size:
+    		pre = size
+    		s = s.replace('()', '').replace('[]', '').replace('{}', '')    		    		
+    		size = len(s)
+
+    	return size == 0
+        
+
+
+s = Solution()
+print(s.isValid("[(]"))
