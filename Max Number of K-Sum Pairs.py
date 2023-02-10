1c65197eaff90ed863b6475b1b950f80df4c0534
KnightDanny <122959926+KnightDanny@users.noreply.github.com>
2023-02-11 02:16:11 +0300

    Update Max Number of K-Sum Pairs.py

--- Max Number of K-Sum Pairs.py
+++ Max Number of K-Sum Pairs.py
@@ -1,20 +1,11 @@
 class Solution(object):
     def maxOperations(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
         tally = 0
         nums.sort()
-
         for x in nums:
-            
             for y in nums:
-                
                 if x + y == k:
                     tally += 1
                     nums.remove(x)
                     nums.remove(y)
-
         return tally
