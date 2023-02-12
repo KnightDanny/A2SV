ae9d156e89c8ad205c5cfd157885746231be3730
KnightDanny <122959926+KnightDanny@users.noreply.github.com>
2023-02-12 15:52:55 +0300

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -2,8 +2,8 @@ class Solution(object):
     def fizzBuzz(self, n):
         output = []
         for i in range(1,n+1):
-            if i % 15 == 0: answer += ["FizzBuzz"]
-            elif i % 5 == 0: answer += ["Buzz"]
-            elif i % 3 == 0: answer += ["Fizz"]
+            if i % 15 == 0: output.append(["FizzBuzz"])
+            elif i % 5 == 0: output.append(["Buzz"])
+            elif i % 3 == 0: output.append(["Fizz"])
             else: output.append([str(i)])
-        return answer
+        return output
